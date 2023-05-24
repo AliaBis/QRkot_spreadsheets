720f8fe294a3b24cb1f39da4893abfacb7e61f03
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2023-05-24 16:08:45 +0300

    table

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,86 +1,54 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from aiogoogle import Aiogoogle
 
 from app.core.config import settings
 
-
-#создать папаку с константами?
 FORMAT = "%Y/%m/%d %H:%M:%S"
-DATE_TIME = datetime.now().strftime(FORMAT)
-TABLE = [
-    ['Отчет на: ', DATE_TIME],
-    ['Проекты по скорости закрытия'],
-    ['Название проекта', 'Время сбора', 'Описание']
-]
-TABLE_BODY = {
-    'properties': {'title': f'Отчет на {DATE_TIME}',
-                   'locale': 'ru_RU'},
-    'sheets': [{'properties': {'sheetType': 'GRID',
-                               'sheetId': 0,
-                               'title': 'Лист1',
-                               'gridProperties': {'rowCount': 100,
-                                                  'columnCount': 11}}}]
-}
 
 
-async def spreadsheets_create(aiogoogle_object: Aiogoogle) -> str:
-    """Создание таблицы"""
-    service = await aiogoogle_object.discover('sheets', 'v4')
-    response = await aiogoogle_object.as_service_account(
-        service.spreadsheets.create(json=TABLE_BODY)
-    )
-    spreadsheet_id = response['spreadsheetId']
-    print(spreadsheet_id)
-    return spreadsheet_id
+async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+    now_date_time = datetime.now().strftime(FORMAT)
+    service = await wrapper_services.discover('sheets', 'v4')
+    spreadsheet_body = {
+        'properties': {'title': f'Отчет на {now_date_time}',
+                       'locale': 'ru_RU'},
+        'sheets': [{'properties': {'sheetType': 'GRID',
+                                   'sheetId': 0,
+                                   'title': 'Лист1',
+                                   'gridProperties': {'rowCount': 100,
+                                                      'columnCount': 11}}}]
+    }
+    response = await wrapper_services.as_service_account(
+        service.spreadsheets.create(json=spreadsheet_body))
+    return response['spreadsheetId']
 
 
 async def set_user_permissions(
-        spreadsheet_id: str,
-        aiogoogle_object: Aiogoogle
-) -> None:
-    """Предоставление прав доступа"""
-    permissions_body = {'type': 'user',
-                        'role': 'writer',
-                        'emailAddress': settings.email}
-    service = await aiogoogle_object.discover('drive', 'v3')
-    await aiogoogle_object.as_service_account(
+        spreadsheetid: str, wrapper_services: Aiogoogle) -> None:
+    permissions_body = {
+        'type': 'user', 'role': 'writer', 'emailAddress': settings.email}
+    service = await wrapper_services.discover('drive', 'v3')
+    await wrapper_services.as_service_account(
         service.permissions.create(
-            fileId=spreadsheet_id,
-            json=permissions_body,
-            fields="id"
-        ))
+            fileId=spreadsheetid, json=permissions_body, fields="id"))
 
 
 async def spreadsheets_update_value(
-        spreadsheet_id: str,
-        projects: list,
-        aiogoogle_object: Aiogoogle
-) -> None:
-    """Запись инфы из базы данных в документ с таблицами"""
-    service = await aiogoogle_object.discover('sheets', 'v4')
-    table_values = TABLE
-    for project in projects:
-        print(project, type(project))
-        new_row = [
-            str(project['name']),
-            str(timedelta(project['_no_label'])),
-            str(project['description'])
-        ]
+        spreadsheetid: str, projects: list,
+        wrapper_services: Aiogoogle) -> None:
+    service = await wrapper_services.discover('sheets', 'v4')
+    table_values = [
+        ['Отчет на: ', datetime.now().strftime(FORMAT)],
+        ['Проекты по скорости закрытия'],
+        ['Название проекта', 'Время сбора', 'Описание']]
+    for res in projects:
+        new_row = [res['name'], str(res['delta']), res['description']]
         table_values.append(new_row)
-    update_body = {
-        'majorDimension': 'ROWS',
-        'values': table_values
-    }
-
-    try:
-        await aiogoogle_object.as_service_account(
-            service.spreadsheets.values.update(
-                spreadsheetId=spreadsheet_id,
-                range='A1:D100',
-                valueInputOption='USER_ENTERED',
-                json=update_body
-            )
-        )
-    except ValueError:
-        print('Проверь входящие данные')
+    update_body = {'majorDimension': 'ROWS', 'values': table_values}
+    await wrapper_services.as_service_account(
+        service.spreadsheets.values.update(
+            spreadsheetId=spreadsheetid,
+            range='A1:E30',
+            valueInputOption='USER_ENTERED',
+            json=update_body))
